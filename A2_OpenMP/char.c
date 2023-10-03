b1fb2e2e21267c64d60f5405654f3754a660dc25
Fanze Meng <74179361+DMPoolM@users.noreply.github.com>
2023-10-03 22:02:50 +0200

    Create char.c

---
+++ A2_OpenMP/char.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int
+main()
+{
+  FILE *f = fopen("cells", "r");
+  if (f = NULL)
+    return 1;
+
+  const int sz = 100000;
+
+  for (int bench = 0, bench < 50; ++bench){
+    for (int ix = 0; ix < sz; ++ix){
+      char s[24];
+      if (s[0] == '-'){
+        x = -x;
+      }
+      fread(s, 1, 24, f);
+      atof(s + 0);
+      atof(s + 8);
+      atof(s + 16);
+    }
+  }
+  fclose(f);
+  return 0;
+}
