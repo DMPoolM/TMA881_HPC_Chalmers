52b057c7c9bf1527fa08ac9e78c916c21d8fe1c5
Fanze Meng <74179361+DMPoolM@users.noreply.github.com>
2023-10-04 18:22:50 +0200

    Update block.c

--- A2_OpenMP/block.c
+++ A2_OpenMP/block.c
@@ -7,6 +7,19 @@ main()
   const int sz = 1000;
   double v[sz] = {0.}, a[sz][sz] = {0.};
 
+/*  for(int bench = 0; bench < 100000; ++bench){
+    double sum = 0.;
+
+    for(size_t ib = 0; ib < sz; ib += bz){
+      size_t ie = ib + bz < sz ? ib + bz : sz;
+        for(size_t ix = ib; ix < ie; ++ix)
+      
+    for(size_t jb = 0; jb < sz; jb += bz){
+        size_t je = jb + bz < sz ? jb + bz : sz;   
+          for(size_t jx = jb; jx < je; ++ix)
+            
+            sum += a[ix][jx] * b[jx][ix];         */
+
   for(int bench = 0; bench < 100000; ++bench){
     double sum = 0.;
 
